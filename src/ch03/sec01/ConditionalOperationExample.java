42c5bfa1d86f6b210238a2750333e09b0d7e3178
yelim <kyl656227@gmail.com>
2026-01-14 14:42:28 +0900

    hi

--- src/ch03/sec01/ConditionalOperationExample.java
+++ src/ch03/sec01/ConditionalOperationExample.java
@@ -5,5 +5,11 @@ public class ConditionalOperationExample {
         int score = (int) (Math.random()*91.0)+10;
         System.out.printf("score:" + score);
 
-    }
+
+        //삼항식을 이용해서 90점 초과는 A 출력, 80점 초과는 B C출력
+        // 나머지는 C출력
+
+        String result = (score > 90 ? "A" : score > 80 ? "B" : "C");
+        System.out.printf("result:" + result);
+     }
 }
